e2d3c0b432780d11eaacd895e1791a5dc1ffe21d
alvarorichard <alvaro10048@protonmail.com>
2023-07-14 00:00:41 -0300

    fix issues

--- main.c
+++ main.c
@@ -1,141 +1,125 @@
-#define STACK_MAX 256
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
 
-typedef enum{
+#define STACK_MAX 256
+#define INITIAL_GC_THRESHOLD 256
+
+typedef enum {
     OBJ_INT,
     OBJ_PAIR,
-
-}ObjectType;
+} ObjectType;
 
 typedef struct sObject {
-  ObjectType type;
+    ObjectType type;
+    unsigned char marked;
+    struct sObject* next;
 
-  union {
-    
-    int value;
+    union {
+        int value;
 
- 
-    struct {
-      struct sObject* head;
-      struct sObject* tail;
+        struct {
+            struct sObject* head;
+            struct sObject* tail;
+        };
     };
-  };
 } Object;
 
-
-typedef struct{
+typedef struct {
     Object* stack[STACK_MAX];
     int stackSize;
-}VM;
+    Object* firstObject;
+    int numObjects;
+    int maxObjects;
+} VM;
 
-VM* newVM(){
+VM* newVM() {
     VM* vm = malloc(sizeof(VM));
-    vm->stackSize=0 ;
+    vm->stackSize = 0;
+    vm->numObjects = 0;
+    vm->maxObjects = INITIAL_GC_THRESHOLD;
     return vm;
 }
 
-void push(VM* vm, Object* value){
-    assert(vm->stackSize<STACK_MAX,"Stack overflow!");
-    vm->stack[vm->stackSize++]=value;
-
+void push(VM* vm, Object* value) {
+    assert(vm->stackSize < STACK_MAX);
+    vm->stack[vm->stackSize++] = value;
 }
 
-Object*pop(VM*vm){
-    assert(vm->stackSize>0,"Stack underflow!");
+Object* pop(VM* vm) {
+    assert(vm->stackSize > 0);
     return vm->stack[--vm->stackSize];
-
 }
 
 Object* newObject(VM* vm, ObjectType type) {
-  Object* object = malloc(sizeof(Object));
-  object-> type = type;
-  return object;
+    if (vm->numObjects == vm->maxObjects) gc(vm);
+
+    Object* object = malloc(sizeof(Object));
+    object->type = type;
+    object->marked = 0;
+
+    object->next = vm->firstObject;
+    vm->firstObject = object;
+
+    vm->numObjects++;
+
+    return object;
 }
 
 void pushInt(VM* vm, int intValue) {
-  Object* object = newObject(vm, OBJ_INT);
-  object->value = intValue;
-  push(vm, object);
+    Object* object = newObject(vm, OBJ_INT);
+    object->value = intValue;
+    push(vm, object);
 }
 
 Object* pushPair(VM* vm) {
-  Object* object = newObject(vm, OBJ_PAIR);
-  object->tail = pop(vm);
-  object->head = pop(vm);
+    Object* object = newObject(vm, OBJ_PAIR);
+    object->tail = pop(vm);
+    object->head = pop(vm);
 
-  push(vm, object);
-  return object;
+    push(vm, object);
+    return object;
 }
 
-// marca marky
-//
-//
-typedef struct sObject{
-  unsigned char marked;
+void mark(Object* object) {
+    if (object->marked) return;
 
-} Object;
-
-
-void markAll(VM* vm){
-for (int i = 0; i < vm-> stackSize; i++){
-    mark(vm->stack [i]);
-  }
-
-}
-
-void markAll (Object* object){
- object->marked = 1;
-}
-
-
-void mark(Object* object ){
-    object->marked = 1; 
-}
-
-void mark (Object* object){
     object->marked = 1;
-    if (object->type == OBJ_PAIR){
+
+    if (object->type == OBJ_PAIR) {
         mark(object->head);
         mark(object->tail);
     }
 }
 
-typedef struct sObject{
-     struct sObject* next;
+void markAll(VM* vm) {
+    for (int i = 0; i < vm->stackSize; i++) {
+        mark(vm->stack[i]);
+    }
+}
 
-} Object;
-
-typedef struct{
-    Object* firstObject;
-
-}VM;
-
-void sweep(VM* vm){
-    Object** object =&vm->firstObject;
-    while(*object){
-        if(!(*object)-> marked){
+void sweep(VM* vm) {
+    Object** object = &vm->firstObject;
+    while (*object) {
+        if (!(*object)->marked) {
             Object* unreached = *object;
             *object = unreached->next;
             free(unreached);
-        }else{
+            vm->numObjects--;
+        } else {
             (*object)->marked = 0;
             object = &(*object)->next;
         }
     }
 }
 
-void gc(VM* vm){
+void gc(VM* vm) {
+    int numObjects = vm->numObjects;
+
     markAll(vm);
     sweep(vm);
-}
- typedef struct 
- {
-    int numObjects;
-    int maxObjects;
- }VM;
 
- VM* newVM(){
-     vm->numObjects = 0;
-     vm->maxObjects = INITIAL_GC_THRESHOLD;
-     return vm;
- }
- 
+    vm->maxObjects = vm->numObjects == 0 ? INITIAL_GC_THRESHOLD : vm->numObjects * 2;
+
+    printf("Collected %d objects, %d remaining.\n", numObjects - vm->numObjects, vm->numObjects);
+}
